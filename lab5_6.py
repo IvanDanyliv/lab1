cda6c4da88ba89d6fe34b5fc17754ad319386f42
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-09-24 12:53:20 +0300

    Create lab5_6.py

---
+++ lab5_6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#!-*- coding: utf-8 -*-
+
+a = int(input('Перша сторона трникутника: '))
+b = int(input('Друга сторона трникутника: '))
+c = int(input('Третя сторона трникутника: '))
+
+if a <= 0 or b <= 0 or c <= 0:
+    print('Трикутник не існує')
+else:
+    if a + b > c and a + c > b and b + c > a:
+        print('Трикутник існує')
+    else:
+        print('Трикутник не існує')
+ 
