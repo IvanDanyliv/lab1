ae682e9ad7d125c9e18cb259bdcfc95ae1c1d185
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-09-24 09:49:48 +0300

    Create lab4_2.py

---
+++ lab4_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import math
+
+a = float(input("Введіть дійсне число: "))
+b = float(input("Введіть дійсне число: "))
+c = float(input("Введіть дійсне число: "))
+
+f=1/(c*math.sqrt(2*math.pi))*math.exp(-((a-b)**2)/(2*c**2))
+
+print(f)
