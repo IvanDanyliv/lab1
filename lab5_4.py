c8bcda0e7d57ad5a775e24554fd092da71b42fe9
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-09-24 11:56:26 +0300

    Create lab5_4.py

---
+++ lab5_4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#!-*- coding: utf-8 -*-
+
+import math
+
+a = float(input('Введіть перший член квадратного рівняння:'))
+b = float(input('Введіть другий член квадратного рівняння:'))
+c = float(input('Введіть третій член квадратного рівняння:'))
+
+if a == 0 and b != 0:
+    x1 = None
+    x2 = -(c / b)
+elif a == 0 and b == 0:
+    x1 = x2 = None
+else:
+    discriminant = b ** 2 - 4 * a * c
+    x1 = (-b + discriminant ** 0.5)/2 * a
+    x2 = (-b - discriminant ** 0.5)/2 * a
+print("x1=" + str(x1) + "\t" + "x2=" + str(x2))
