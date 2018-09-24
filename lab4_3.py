4245d3b303a4588a473685105766c57ed509bf05
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-09-24 10:25:47 +0300

    Create lab4_3.py

---
+++ lab4_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#!-*- coding: utf-8 -*-
+
+import decimal
+wage = decimal.Decimal(input('Введіть місячний дохід:'))
+p_tax = decimal.Decimal('0.18')
+m_tax = decimal.Decimal('0.015')
+
+full_tax = wage * (p_tax + m_tax)
+
+print('Сума податку складає: ' + str(full_tax))
