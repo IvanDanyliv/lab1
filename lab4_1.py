48d5b645673661b114de2898e129742e16d013ce
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-09-24 09:48:10 +0300

    Create lab4_1.py

---
+++ lab4_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+
+a = float(input("Введіть невід'ємне число: "))
+b = float(input("Введіть додатнє число: "))
+
+x=(math.sqrt(a*b))/((math.e**a)*b)+(a*math.e**((2*a)/b))
+
+print(x)
