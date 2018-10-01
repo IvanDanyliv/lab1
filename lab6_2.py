9b1ab5e90c7dcc38f1379e8f095c27cd35d91355
IvanDanyliv <31760860+IvanDanyliv@users.noreply.github.com>
2018-10-01 12:21:47 +0300

    Create lab6_2.py

---
+++ lab6_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#!-*- coding: utf-8 -*-
+
+import math
+
+def ui_input() -> list:
+    #Input of argument
+    a = float(input("Available amount "))
+    b = float(input("Interest rate "))
+    c = float(input("Duration of the deposit "))
+    return[a, b, c]
+
+def ui_output(money: float):
+    #Output of result
+    print (money)
+
+def calculate_area_triangle(deposit: list) -> float:
+    #Calculate final amout of money deposit
+    final_money = deposit[0]*(1+deposit[1]/100)**deposit[2]
+    return final_money
+
+ui_output(calculate_area_triangle(ui_input()))
